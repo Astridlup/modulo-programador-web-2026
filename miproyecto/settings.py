7bc1fa903e8c4c103a1d813b06ffdd573d42ec3d
astridlup <astrita1970@gmail.com>
2026-04-01 16:47:20 -0300

    Conexion a la BD MySQL

--- miproyecto/settings.py
+++ miproyecto/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'miproyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproyecto_db',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
